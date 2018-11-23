eb9ce17466dd20fe7a63f27868b059ef83384e96
xiao11lang <1023819715@qq.com>
2018-11-23 17:02:28 +0800

    邀约模块发布、删除前端及后台接口

--- src/component/appoint/allAppoint.js
+++ src/component/appoint/allAppoint.js
@@ -1,65 +1,81 @@
 import React, { Component } from 'react';
 import {Avatar,Button,Divider,Table} from 'antd'
-const columns = [{
-    title: '昵称',
-    dataIndex: 'nickName',
-    key: 'nickName',
-    align:'center',
-    render:(nickName,record)=><><Avatar src={record.avatarUrl}/>
-    <Divider type="vertical" />
-    <span>{nickName}</span></>
-  }, {
-    title: '主题',
-    dataIndex: 'theme',
-    key: 'theme',
-    align:'center',
-  }, {
-    title: '期待的人',
-    dataIndex: 'want',
-    key: 'want',
-    align:'center'
-  }, {
-    title: '详情',
-    key: 'detail',
-    dataIndex: 'detail',
-    align:'center'
-  }, {
-    title: '时间',
-    key: 'appointTime',
-    dataIndex:'appointTime',
-    align:'center'
-  },{
-      title: '地点',
-      key: 'appointLocation',
-      dataIndex:'appointLocation',
-      align:'center'
-    },{
-        title: '发布时间',
-        key: 'writeTime',
-        dataIndex:'writeTime',
+class All extends Component{
+    columns=[{
+        title: '昵称',
+        dataIndex: 'nickName',
+        key: 'nickName',
+        align:'center',
+        render:(nickName,record)=><><Avatar src={record.avatarUrl}/>
+        <Divider type="vertical" />
+        <span>{nickName}</span></>
+      }, {
+        title: '主题',
+        dataIndex: 'theme',
+        key: 'theme',
+        align:'center',
+      }, {
+        title: '期待的人',
+        dataIndex: 'want',
+        key: 'want',
+        align:'center'
+      }, {
+        title: '详情',
+        key: 'detail',
+        dataIndex: 'detail',
+        align:'center'
+      }, {
+        title: '时间',
+        key: 'appointTime',
+        dataIndex:'appointTime',
         align:'center'
       },{
-        title: '操作',
-        key: 'action',
-        align:'center',
-        render: () => <>
-            <Button type='primary'>
-            发布
-            </Button>
-            <Divider type="vertical" />
-            <Button type='danger'>
-            删除
-            </Button>
-        </>,
-      }];
-class All extends Component{
+          title: '地点',
+          key: 'appointLocation',
+          dataIndex:'appointLocation',
+          align:'center'
+        },{
+            title: '发布时间',
+            key: 'writeTime',
+            dataIndex:'writeTime',
+            align:'center'
+          },{
+            title: '操作',
+            key: 'action',
+            align:'center',
+            render: (action,record) => <>
+                <Button type='primary' onClick={()=>this.props.publish(record.id)}>
+                发布
+                </Button>
+                <Divider type="vertical" />
+                <Button type='danger' onClick={()=>this.props.deleteAppoint(record.id)}>
+                删除
+                </Button>
+            </>
+          }];
     render(){
+        /* if(this.props.user.level>=2&&this.columns.length===7){
+            this.columns.push({
+                title: '操作',
+                key: 'action',
+                align:'center',
+                render: (action,record) => <>
+                    <Button type='primary' onClick={()=>this.props.publish(record.id)}>
+                    发布
+                    </Button>
+                    <Divider type="vertical" />
+                    <Button type='danger' onClick={()=>this.props.deleteAppoint(record.id)}>
+                    删除
+                    </Button>
+                </>
+              })
+        } */
         let data=this.props.all.map((user)=>{
             return Object.assign({},user,{key:user.openid})
         });
         return (
             <>
-                <Table columns={columns} dataSource={data} />
+                <Table columns={this.columns} dataSource={data} />
             </>
         )
     }
--- src/component/appoint/appoint.js
+++ src/component/appoint/appoint.js
@@ -3,22 +3,49 @@ import LeftMenu from '../until/menu'
 import axios from 'axios'
 import './apponit.scss'
 import All from './allAppoint'
+import PublishAppoint from './publishAppoint'
 class Appoint extends Component{
     constructor(){
         super();
         this.state={
             items:['全部邀约','已发布'],
-            all:[]
+            activeKey:'0',
+            allAppoint:[],
+            publishAppoint:[],
+            dataGet:false
         }
         this.toggle=this.toggle.bind(this)
     }
     componentDidMount(){
         axios.get('http://www.11lang.cn/mp/appoint').then((res)=>{
             this.setState({
-                all:res.data,
-                activeKey:'0'
+                allAppoint:res.data.appoint,
+                publishAppoint:res.data.appoint.filter((appoint)=>appoint.publish===1),
+                dataGet:true
             })
-            console.log(res.data)
+        })
+    }
+    publish=(id)=>{
+        if(this.state.publishAppoint.find((appoint)=>appoint.id===id)){
+            alert('此条已经发布')
+            return
+        }
+        let fd=new FormData();
+        fd.append('id',id)
+        axios.post('http://www.11lang.cn/mp/publishAppoint',fd).then(()=>{
+            this.setState({
+                publishAppoint:[this.state.allAppoint.find((lo)=>lo.id===id),...this.state.allAppoint]
+            })
+        })
+    }
+    deleteAppoint=(id)=>{
+        let fd=new FormData();
+        fd.append('id',id)
+        axios.post('http://www.11lang.cn/mp/deleteAppoint',fd).then(()=>{
+            this.setState({
+                allAppoint:this.state.allAppoint.filter((appoint)=>appoint.id===id?null:lo),
+                publishLove:this.state.allAppoint.filter((appoint)=>appoint.publish===1)
+            })
         })
     }
     toggle(key){
@@ -31,8 +58,9 @@ class Appoint extends Component{
             <div className='appoint'>
                 <LeftMenu items={this.state.items} toggle={this.toggle}></LeftMenu>
                 <div className='appointMain'>
-                    {this.state.activeKey==='0'?
-                        <All all={this.state.all}></All>:<div>publish</div>
+                    {this.state.dataGet?(this.state.activeKey==='0'?
+                        <All all={this.state.allAppoint} publish={this.publish} deleteAppoint={this.deleteAppoint}></All>:
+                        <PublishAppoint publishAppoint={this.state.publishAppoint}></PublishAppoint>):null
                     }
                 </div>
             </div>
