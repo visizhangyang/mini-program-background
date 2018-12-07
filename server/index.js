0fa231b5b2ca5d372716162aa0f1af3ff330eeaf
xiao11lang <1023819715@qq.com>
2018-12-07 14:29:48 +0800

    增加koa日志记录

--- server/index.js
+++ server/index.js
@@ -2,6 +2,19 @@ const Koa=require('koa')
 const koaBody = require('koa-body');
 const router=require('./routes/route')
 var app=new Koa()
+const logUtil = require('./log');
+app.use(async (ctx, next) => {
+  const start = new Date();
+  var ms;
+  try {
+    await next();
+    ms = new Date() - start;
+    logUtil.logResponse(ctx, ms);
+  } catch (error) {
+    ms = new Date() - start;
+    logUtil.logError(ctx, error, ms);
+  }
+});
 app.use(koaBody({
     multipart:true
 }))
